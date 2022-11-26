5d76a014dfad498d70b3af2b623a2647d0f0e909
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-11-26 13:28:37 +0700

    install mongoose

--- app.js
+++ app.js
@@ -1,8 +1,11 @@
 const express = require('express');
+const mongoose = require('mongoose');
 
 const app = express();
 
 const { PORT = 3000 } = process.env;
+
+mongoose.connect('mongodb://localhost:27017/bitfilmsdb');
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}`);
