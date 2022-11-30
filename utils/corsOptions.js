91b2343a1afbf300f572b843a7f4aa4128eb7f92
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-11-30 22:29:43 +0700

    create corsOption constantant

---
+++ utils/corsOptions.js
@@ -0,0 +1,7 @@
+const corsOptions = {
+  // origin: ['http://mesto.lizababicheva.nomoredomains.icu',
+  // 'https://mesto.lizababicheva.nomoredomains.icu'],
+  credentials: true,
+};
+
+module.exports = { corsOptions };
