0c782a611cc83263d805a40524839ad41fabba47
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-12-03 17:07:34 +0700

    correct email regex

--- routes/users.js
+++ routes/users.js
@@ -29,7 +29,7 @@ routerUsers.patch('/users/me', celebrate({
   body: Joi.object().keys({
     email: Joi.string().required().email({ minDomainSegments: 2, tlds: { allow: false } })
       .pattern(emailRegEx),
-    name: Joi.string().min(2).max(30),
+    name: Joi.string().required().min(2).max(30),
   }),
 }), auth, updateUser);
 
--- utils/constants.js
+++ utils/constants.js
@@ -1,6 +1,6 @@
 const urlRegEx = /^(https?:\/\/)?([\w-]{1,32}\.[\w-]{1,32})[^\s@]*$/;
 const numRegEx = /^\d+$/;
-const emailRegEx = /^[a-zA-Z0-9_.]+[@]{1}[a-z0-9]+\.[a-z]+$/;
+const emailRegEx = /^[a-zA-Z0-9_.]+[@]{1}[a-zA-Z0-9]+\.[a-zA-Z]+$/;
 
 const unauthorizedErrMessage = 'Необходима авторизация';
 const badReqErrMessage = 'Переданы некорректные данные';
