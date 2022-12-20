3ed806f0105d1dfc7906cee6c120027fb94fc120
Elizaveta Babicheva <elizavetababicheva@yandex.com>
2022-12-20 19:07:29 +0700

    add localhost to Cors

--- utils/corsOptions.js
+++ utils/corsOptions.js
@@ -1,6 +1,7 @@
 const corsOptions = {
   origin: ['http://films.lizab.nomoredomains.club',
-    'https://films.lizab.nomoredomains.club'],
+    'https://films.lizab.nomoredomains.club',
+    'http://localhost:3000/'],
   credentials: true,
 };
 
